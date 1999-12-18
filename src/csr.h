eb99eec7ab20c9b0fc2d9a60359fac89e85f33bd
abombe <abombe@53a565d1-3bb7-0310-b661-cf11e63c67ab>
1999-12-18 16:02:15 +0000

    - Added FCP register addresses - Added #ifdef for multiple inclusion
    git-svn-id: svn://svn.linux1394.org/libraw1394/trunk@3 53a565d1-3bb7-0310-b661-cf11e63c67ab

--- src/csr.h
+++ src/csr.h
@@ -1,3 +1,5 @@
+#ifndef _LIBRAW1394_CSR_H
+#define _LIBRAW1394_CSR_H
 
 #define CSR_REGISTER_BASE  0xfffff0000000ULL
 
@@ -17,7 +19,12 @@
 #define CSR_CHANNELS_AVAILABLE_LO 0x228
 #define CSR_CONFIG_ROM            0x400
 #define CSR_CONFIG_ROM_END        0x800
+#define CSR_FCP_COMMAND           0xB00
+#define CSR_FCP_RESPONSE          0xD00
+#define CSR_FCP_END               0xF00
 #define CSR_TOPOLOGY_MAP          0x1000
 #define CSR_TOPOLOGY_MAP_END      0x1400
 #define CSR_SPEED_MAP             0x2000
 #define CSR_SPEED_MAP_END         0x3000
+
+#endif /* _LIBRAW1394_CSR_H */
