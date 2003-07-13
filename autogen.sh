27d115782e482e5b74c32fa0ec6234582c8baf97
bencollins <bencollins@53a565d1-3bb7-0310-b661-cf11e63c67ab>
2003-07-13 01:27:13 +0000

    Ooops...libtool works a bit different than I thought, but atleast it works like it is supposed to.
    git-svn-id: svn://svn.linux1394.org/libraw1394/trunk@117 53a565d1-3bb7-0310-b661-cf11e63c67ab

--- autogen.sh
+++ autogen.sh
@@ -45,11 +45,6 @@ test $TEST_TYPE $FILE || {
         exit 1
 }
 
-if test -z "$*"; then
-        echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
 case $CC in
 *xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
 esac
